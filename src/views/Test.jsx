d51d743e693a3de6a5c35641adf3d03cf4bf81a0
kornel-odoj <kornelodoj.4@gmail.com>
2023-03-16 13:20:17 +0100

    Create Button.jsx
    Create button component

--- src/views/Test.jsx
+++ src/views/Test.jsx
@@ -1,4 +1,5 @@
 import Form from "../components/Form";
+import Button from '../components/Button';
 
 const Test = () => {
     return (
@@ -8,6 +9,15 @@ const Test = () => {
 
             <p>----------</p>
             <Form showBtn={true} />
+            <Button outline={true} size="sm">
+                Click me!
+            </Button>
+            <Button outline={false} size="md">
+                Click me!
+            </Button>
+            <Button outline={true} size="lg">
+                Click me!
+            </Button>
         </main>
     );
 }

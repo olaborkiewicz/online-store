768cc8c0fd588acd0c1e0e388c61a2b11743d561
AshmaDev <ashmadev@protonmail.com>
2023-03-16 19:53:17 +0100

    useState, useEffect

--- src/views/Landing.jsx
+++ src/views/Landing.jsx
@@ -1,6 +1,15 @@
+import { useEffect } from 'react';
 import Navbar from '../components/Navbar';
 
 const Landing = () => {
+    useEffect(()=> {
+        document.body.style.backgroundColor = `rgb(0, 0, 0)`;
+
+        return () => {
+            document.body.style.backgroundColor = `rgb(255, 0, 0)`;
+        }
+    }, []);
+
     return (
         <main>
             <Navbar />
--- src/views/Test.jsx
+++ src/views/Test.jsx
@@ -1,13 +1,32 @@
-import Form from "../components/Form";
+import { useState, useEffect } from 'react';
+
+import TestCounter from '../components/TestComponents/TestCounter';
+import TestSelect from '../components/TestComponents/TestSelect';
+import TestSearch from '../components/TestComponents/TestSearch';
+
+import styles from './Test.module.css';
 
 const Test = () => {
-    return (
-        <main>
-            <h1>Test</h1>
-            <Form />
+    const [counter, setCounter] = useState(0);
 
-            <p>----------</p>
-            <Form showBtn={true} />
+    useEffect(() => {
+        alert("Komponent został zmontowany!")
+    }, []);   
+
+    return (
+        <main className={styles.container}>
+            <div className={styles.innerBlock}>
+                <h1>Test 1 - Counter</h1>
+                <TestCounter counter={counter} setCounter={setCounter} />
+            </div>
+            <div className={styles.innerBlock}>
+                <h1>Test 2 - Select</h1>
+                <TestSelect counter={counter} />
+            </div>
+            <div className={styles.innerBlock}>
+                <h1>Test 3 - Search</h1>
+                <TestSearch />
+            </div>
         </main>
     );
 }

64a4caa62a5ff09329b57b904fe14d998b628e75
Mateusz <108770502+musior@users.noreply.github.com>
2023-03-17 03:45:16 +0100

    Editing nav component
    Adding company logo and shopping cart.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,19 +2,25 @@ import { Link } from "react-router-dom";
 import { links } from "../data/routes";
 
 const Navbar = () => {
-    return (
-        <nav>
-            <ul>
-                {
-                    links.map((item) => (
-                        <li key={`route_${item.name}`} className="nav-item">
-                            <Link to={item.path}>{item.name}</Link>
-                        </li>
-                    ))
-                }
-            </ul>
-        </nav>
-    );
-}
+  let amountProduct = 0;
+  return (
+    <nav className="navigation">
+      <img src="" alt="company-logo" className="navigation-logo" />
+      <ul className="navigation-list">
+        {links.map((item) => (
+          <li key={`route_${item.name}`} className="navigation-list-item">
+            <Link to={item.path}>{item.name}</Link>
+          </li>
+        ))}
+      </ul>
+      <div className="navigation-cart">
+        <a href="/">
+          <img src="" alt="shopping-cart" className="navigation-cart-icon" />
+        </a>
+        <span className="amountOfProducts">{amountProduct}</span>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
